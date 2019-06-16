bad71d9dc17e18a390e239a6e1dd10a083b7d65a
ivomastre <40743787+ivomastre@users.noreply.github.com>
2019-06-16 06:53:27 -0300

    Add files via upload

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 const { Permissions } = require('discord.js');
 const permissions = new Permissions(1207959552);
 const http = require('http');
-const appJS = require('app.js');
+const appJS = require('app');
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.end("Hello World\n");
